bcbda2b832a5f162c1097dd9aaae7ca29812432b
ssyangchn <ssyangchn@gmail.com>
2025-12-11 22:42:58 +0800

    init

---
+++ Module/zhihu.js
@@ -0,0 +1,11 @@
+let body = $response.body;
+if (body) {
+    try {
+        let json = JSON.parse(body);
+        if (json.data && Array.isArray(json.data)) {
+            json.data = json.data.filter(i => i.type !== "ad");
+        }
+        body = JSON.stringify(json);
+    } catch (e) {}
+}
+$done({ body });
