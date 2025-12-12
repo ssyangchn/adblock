063e45812832054d8a2b9a775f8655d227cb347c
ssyangchn <ssyangchn@gmail.com>
2025-12-12 13:06:32 +0800

    update

--- Module/zhihu.js
+++ Module/zhihu.js
@@ -1,10 +1,15 @@
 let body = $response.body;
 
 if (body) {
+    $notification.post("开始去广告");
     try {
         let json = JSON.parse(body);
         if (json.data && Array.isArray(json.data)) {
+            let before = json.data.length;
             json.data = json.data.filter(i => i.type !== "ad");
+            let after = json.data.length;
+            let removed = before - after;
+            $notification.post(`已过滤 ${removed} 条广告`);
         }
         body = JSON.stringify(json);
     } catch (e) {}
