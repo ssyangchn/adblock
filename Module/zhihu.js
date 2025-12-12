7c46078f9eb0162240fb9f90446379519e002ad5
ssyangchn <ssyangchn@gmail.com>
2025-12-12 14:15:19 +0800

    去掉日志

--- Module/zhihu.js
+++ Module/zhihu.js
@@ -4,13 +4,7 @@ if (body) {
     try {
         let json = JSON.parse(body);
         if (json.data && Array.isArray(json.data)) {
-            let before = json.data.length;
             json.data = json.data.filter(i => i.type !== "ad");
-            let after = json.data.length;
-            let removed = before - after;
-            if (removed > 0) {
-                $notification.post("知乎去广告","",`已过滤 ${removed} 条广告`);
-            }
         }
         body = JSON.stringify(json);
     } catch (e) {}
